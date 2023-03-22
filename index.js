a77f26c53595da5f00985913bb420e018070b3eb
Eng-NUREDDIN <nour.hammoda@gmail.com>
2023-03-22 03:40:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record){
+   const result=record.find(isWin);
+  return result==undefined? undefined:result.year
+}
+function isWin(element, index, array) {
+    if (element["result"] === "W") {
+      return element["year"];
+    } else {
+      return undefined;
+    }
+  }
